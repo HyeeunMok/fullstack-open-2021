cd3a722858101206cfe4f44ff54a24992f1dee4e
Hyeeun Mok <hyeeuni.mok@gmail.com>
2021-05-20 22:13:07 -0400

    part2.4 - before refactoring

--- part2/course-info/src/App.js
+++ part2/course-info/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
-const Header = ({ course }) => {
-  return <h1>{course.name}</h1>;
-};
+const Header = () => <h1>Web development curriculum</h1>;
+
+const Title = ({ course }) => <h2>{course.name}</h2>;
 
 const Total = ({ course }) => {
-  const sum =
-    course.parts[0].exercises +
-    course.parts[1].exercises +
-    course.parts[2].exercises;
-  return <p>Number of exercises {sum}</p>;
+  const total = course.parts.reduce((acc, part) => {
+    return acc + part.exercises;
+  }, 0);
+  return (
+    <p>
+      <strong>total of exercises {total}</strong>
+    </p>
+  );
 };
 
 const Part = props => {
@@ -20,42 +23,86 @@ const Part = props => {
   );
 };
 
-const Content = ({ course }) => {
+const Content1 = ({ course }) => {
   return (
     <div>
       <Part part={course.parts[0]} />
       <Part part={course.parts[1]} />
       <Part part={course.parts[2]} />
+      <Part part={course.parts[3]} />
+    </div>
+  );
+};
+const Content2 = ({ course }) => {
+  return (
+    <div>
+      <Part part={course.parts[0]} />
+      <Part part={course.parts[1]} />
+    </div>
+  );
+};
+
+const Course = ({ courses }) => {
+  return (
+    <div>
+      <Header />
+      <Title course={courses[0]} />
+      <Content1 course={courses[0]} />
+      <Total course={courses[0]} />
+      <Title course={courses[1]} />
+      <Content2 course={courses[1]} />
+      <Total course={courses[1]} />
     </div>
   );
 };
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-      },
-    ],
-  };
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  );
+  return <Course courses={courses} />;
 };
 
 export default App;
