2ebd1ff61675f525d7d20407ea6dc390ebddb616
Hyeeun Mok <hyeeuni.mok@gmail.com>
2021-06-11 23:47:39 -0400

    add cors

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 morgan.token('jsonBody', (req, res) => JSON.stringify(req.body));
 
