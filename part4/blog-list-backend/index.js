7a121ffaa2c30b85a31dff2f09a0523f930e3bc5
Hyeeun Mok <hyeeuni.mok@gmail.com>
2021-07-19 11:23:52 -0400

    finish 4.1

--- part4/blog-list-backend/index.js
+++ part4/blog-list-backend/index.js
@@ -1,1 +1,55 @@
-console.log('hello world');
+// const http = require('http');
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const url = process.env.MONGODB_URI;
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+console.log('connecting to', url);
+
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log('connected to MongoDB');
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then(blogs => {
+    response.json(blogs);
+  });
+});
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog.save().then(result => {
+    response.status(201).json(result);
+  });
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
