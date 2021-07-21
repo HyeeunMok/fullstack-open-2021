67c6610b46162d045eec566ed2bf81572474b764
Hyeeun Mok <hyeeuni.mok@gmail.com>
2021-07-21 14:21:17 -0400

    finish 4.4

--- part4/blog-list-backend/utils/list_helper.js
+++ part4/blog-list-backend/utils/list_helper.js
@@ -1,5 +1,13 @@
 const dummy = blogs => 1;
 
+const totalLikes = blogs => {
+  const total = blogs.reduce((acc, curr) => {
+    return acc + curr.likes;
+  }, 0);
+  return total;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
