bcec9098ac6a1e7767245e1ab432e096ea2b6663
Hyeeun Mok <hyeeuni.mok@gmail.com>
2021-07-28 15:04:50 -0400

    finish 4.8

---
+++ part4/blog-list-backend/tests/blog_api.test.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
+const helper = require('./test_helper');
+const Blog = require('../models/blog');
+
+const api = supertest(app);
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+
+  let blogObject = new Blog(helper.initialBlogs[0]);
+  await blogObject.save();
+  blogObject = new Blog(helper.initialBlogs[1]);
+  await blogObject.save();
+  blogObject = new Blog(helper.initialBlogs[2]);
+  await blogObject.save();
+});
+
+describe('GET /api/blogs', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  });
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs');
+    expect(response.body).toHaveLength(helper.initialBlogs.length);
+  });
+
+  test('a specific blog is within the returned blogs', async () => {
+    const response = await api.get('/api/blogs');
+    const titles = response.body.map(r => r.title);
+    expect(titles).toContain('Go To Statement Considered Harmful');
+  });
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
