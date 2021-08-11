9de39f5ac33357801c73bc7da7b9956d4462569e
Hyeeun Mok <hyeeuni.mok@gmail.com>
2021-08-10 20:43:34 -0400

    finish 4.18

--- part4/blog-list-backend/controllers/blogs.js
+++ part4/blog-list-backend/controllers/blogs.js
@@ -1,7 +1,16 @@
 const Blog = require('../models/blog');
 const User = require('../models/user');
 const logger = require('../utils/logger');
+const jwt = require('jsonwebtoken');
 const blogsRouter = require('express').Router();
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.toLowerCase().startsWith('bearer')) {
+    return authorization.substring(7);
+  }
+  return null;
+};
 
 // Get all blogs
 blogsRouter.get('/', async (request, response) => {
@@ -19,7 +28,15 @@ blogsRouter.get('/:id', async (request, response) => {
 // Add new blog
 blogsRouter.post('/', async (request, response) => {
   const body = request.body;
-  const user = await User.findById(body.userId);
+  const token = getTokenFrom(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid ' });
+  }
+  const user = await User.findById(decodedToken.id);
+  // body.userId === undefined
+  //   ? await User.findOne({})
+  //   : await User.findById(body.userId);
 
   const blog = new Blog({
     title: body.title,
