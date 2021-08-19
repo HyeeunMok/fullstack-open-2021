0d0d2c78ef6615eb31425f6bb47fa849b2a73547
Hyeeun Mok <hyeeuni.mok@gmail.com>
2021-08-19 12:19:45 -0400

    finish 5.1

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,24 +1,58 @@
-import React, { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import React, { useState, useEffect } from 'react';
+import blogService from './services/blogs';
+import loginService from './services/login';
+import LoginForm from './components/LoginForm';
+import BlogList from './components/BlogList';
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    blogService.getAll().then(blogs => setBlogs(blogs));
+  }, []);
+
+  const handleLogin = async event => {
+    event.preventDefault();
+    console.log('logging in with', username, password);
+    try {
+      const user = await loginService.login({ username, password });
+      console.log('user: ', user);
+      setUser(user);
+      setUsername('');
+      setPassword('');
+    } catch (exception) {
+      console.log('wrong credentials', exception);
+    }
+  };
+
+  const handleUsernameChange = event => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = event => {
+    setPassword(event.target.value);
+  };
 
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
+      {user === null ? (
+        <LoginForm
+          handleLogin={handleLogin}
+          username={username}
+          password={password}
+          handleUsernameChange={handleUsernameChange}
+          handlePasswordChange={handlePasswordChange}
+        />
+      ) : (
+        <div>
+          <BlogList blogs={blogs} user={user} />
+        </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
