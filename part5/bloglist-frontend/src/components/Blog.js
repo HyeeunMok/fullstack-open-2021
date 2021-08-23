58e4716d5793434b1f7fa7c33ed272a85ca8a552
Hyeeun Mok <hyeeuni.mok@gmail.com>
2021-08-23 17:51:29 -0400

    finish ex. 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, Fragment } from 'react';
+import styles from './Blog.module.css';
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+  const toggleShow = () => {
+    setShowDetails(!showDetails);
+  };
+
+  return (
+    <div className={styles.blogStyle}>
+      {blog.title} by {blog.author}
+      <button onClick={toggleShow}>{showDetails ? 'Hide' : 'Show'}</button>
+      {showDetails && (
+        <>
+          <p>
+            {blog.url}
+            <br />
+            likes: {blog.likes}
+            <button>likes</button>
+            <br />
+            {blog.user.name}
+          </p>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Blog;
