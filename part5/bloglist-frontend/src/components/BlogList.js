50eaf97e9eca6a9c01ae82a9e7a9292528c517ae
Hyeeun Mok <hyeeuni.mok@gmail.com>
2021-08-24 10:46:50 -0400

    finish ex. 5.9

--- part5/bloglist-frontend/src/components/BlogList.js
+++ part5/bloglist-frontend/src/components/BlogList.js
@@ -4,9 +4,11 @@ import Blog from './Blog';
 const BlogList = ({ blogs, updateBlog }) => {
   return (
     <div>
-      {blogs.map(blog => (
-        <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />
-      ))}
+      {blogs
+        .sort((min, max) => max.likes - min.likes) // Descending order
+        .map(blog => (
+          <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />
+        ))}
     </div>
   );
 };
