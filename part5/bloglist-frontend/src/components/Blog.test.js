bc37d0a0063e137fd7d1f8b2b9d036e7c45960d5
Hyeeun Mok <hyeeuni.mok@gmail.com>
2021-08-24 20:38:40 -0400

    finish ex. 5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Blog from './Blog';
 
-test('renders blogs with only title and author', () => {
+describe('Blog component', () => {
   const blog = {
     title: 'Climate Change',
     author: 'John Doe',
@@ -15,9 +15,25 @@ test('renders blogs with only title and author', () => {
     },
   };
 
-  const component = render(<Blog blog={blog} />);
-  expect(component.container).toHaveTextContent('Climate Change');
-  expect(component.container).toHaveTextContent('John Doe');
-  expect(component.container.likes).toBeUndefined;
-  expect(component.container.url).toBeUndefined;
+  let component;
+  beforeEach(() => {
+    component = render(<Blog blog={blog} />);
+  });
+
+  test('renders blogs with only title and author', () => {
+    expect(component.container).toHaveTextContent('Climate Change');
+    expect(component.container).toHaveTextContent('John Doe');
+    expect(component.container.likes).toBeUndefined;
+    expect(component.container.url).toBeUndefined;
+  });
+
+  test('renders blogs with all details when View button is clicked', () => {
+    const button = component.getByText('View');
+    fireEvent.click(button);
+
+    const blogDetails = component.container.querySelector('.blogDetails');
+    expect(blogDetails).toBeVisible();
+    expect(blogDetails).toHaveTextContent(blog.likes);
+    expect(blogDetails).toHaveTextContent(blog.url);
+  });
 });
