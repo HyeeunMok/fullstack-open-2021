d638be06a84b9f8a0ca467e1d318f9af8a4f6719
Hyeeun Mok <hyeeuni.mok@gmail.com>
2021-11-25 10:51:04 -0500

    finish 7.1

---
+++ part7/routed-anecdotes/src/components/Footer.js
@@ -0,0 +1,23 @@
+const Footer = () => (
+  <div>
+    Anecdote app for{' '}
+    <a
+      href="https://courses.helsinki.fi/fi/tkt21009"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Full Stack -websovelluskehitys
+    </a>
+    . See{' '}
+    <a
+      href="https://github.com/HyeeunMok/fullstack-open-2021/tree/main/part7/routed-anecdotes/src"
+      target="_blank"
+      rel="noreferrer"
+    >
+      https://github.com/HyeeunMok/fullstack-open-2021/tree/main/part7/routed-anecdotes/src
+    </a>{' '}
+    for the source code.
+  </div>
+);
+
+export default Footer;
