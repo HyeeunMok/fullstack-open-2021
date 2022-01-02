b92aba90e9008b7773d22487ef493e3ee872ab43
Hyeeun Mok <hyeeuni.mok@gmail.com>
2022-01-01 19:10:11 -0500

    add blog backend for 7f exercises

---
+++ part7/bloglist-backend/app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+require('express-async-errors');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const middleware = require('./utils/middleware');
+const logger = require('./utils/logger');
+const config = require('./utils/config');
+const blogRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
+const { errorHandler } = require('./utils/error');
+
+console.log('connecting to', config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    logger.info('connected to MongoDB');
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message);
+  });
+
+app.use(cors());
+app.use(express.static('build'));
+app.use(express.json());
+app.use(middleware.morganLogger());
+app.use(middleware.tokenExtractor);
+app.use('/api/blogs', blogRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
+app.use(errorHandler);
+
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing');
+  app.use('/api/testing', testingRouter);
+}
+
+module.exports = app;
