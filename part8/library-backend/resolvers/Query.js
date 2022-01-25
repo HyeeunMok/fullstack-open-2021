6ceda90a9e41d299253f7ccc4ce1eb8575749098
Hyeeun Mok <hyeeuni.mok@gmail.com>
2022-01-25 17:08:03 -0500

    finish ex. 8.2

--- part8/library-backend/resolvers/Query.js
+++ part8/library-backend/resolvers/Query.js
@@ -2,4 +2,8 @@ exports.Query = {
   bookCount: (parent, args, { db }) => db.books.length,
 
   authorCount: (parent, args, { db }) => db.authors.length,
+
+  allBooks: (parent, args, { db }) => {
+    return db.books;
+  },
 };
--- part8/library-backend/schema.js
+++ part8/library-backend/schema.js
@@ -4,5 +4,13 @@ exports.typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
+  }
+
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    genres: [String]
   }
 `;
