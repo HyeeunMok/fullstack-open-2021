6ada664c8cc1bb5bfa152f1d0c245f451d2a9866
Hyeeun Mok <hyeeuni.mok@gmail.com>
2022-01-25 21:33:32 -0500

    finish ex. 8.5

--- part8/library-backend/resolvers/Query.js
+++ part8/library-backend/resolvers/Query.js
@@ -6,8 +6,20 @@ exports.Query = {
   allBooks: (parent, { filter }, { db }) => {
     let filteredBooks = db.books;
     if (filter) {
-      const { author } = filter;
-      filteredBooks = filteredBooks.filter(book => book.author === author);
+      const { author, genre } = filter;
+      if (author !== undefined && genre !== undefined) {
+        filteredBooks = filteredBooks
+          .filter(book => book.author === author)
+          .filter(book => book.genres.includes(genre));
+      }
+      if (author !== undefined) {
+        filteredBooks = filteredBooks.filter(book => book.author === author);
+      }
+      if (genre !== undefined) {
+        filteredBooks = filteredBooks.filter(book =>
+          book.genres.includes(genre)
+        );
+      }
     }
     return filteredBooks;
   },
--- part8/library-backend/schema.js
+++ part8/library-backend/schema.js
@@ -24,5 +24,6 @@ exports.typeDefs = gql`
 
   input BooksFilterInput {
     author: String
+    genre: String
   }
 `;
