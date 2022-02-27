818bc1468517ad44913968ea90bb339c6eb8aad5
Hyeeun Mok <hyeeuni.mok@gmail.com>
2022-02-28 08:13:48 +0900

    finish ex.8.9

--- part8-new/library-frontend/src/components/Books.js
+++ part8-new/library-frontend/src/components/Books.js
@@ -1,9 +1,16 @@
-const Books = (props) => {
-  if (!props.show) {
-    return null
-  }
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import { ALL_BOOKS } from '../queries';
 
-  const books = []
+const Books = props => {
+  const books = useQuery(ALL_BOOKS);
+
+  if (!props.show) {
+    return null;
+  }
+  if (books.loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
@@ -16,7 +23,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {books.data.allBooks.map(a => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author}</td>
@@ -26,7 +33,7 @@ const Books = (props) => {
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default Books
+export default Books;
