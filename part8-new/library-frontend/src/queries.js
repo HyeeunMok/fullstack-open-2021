0b99eb4b9d9debf3ba4c93e35e556fe6dd3b2023
Hyeeun Mok <hyeeuni.mok@gmail.com>
2022-03-01 11:11:27 +0900

    finish ex.8.13-15

--- part8-new/library-frontend/src/queries.js
+++ part8-new/library-frontend/src/queries.js
@@ -14,7 +14,9 @@ export const ALL_BOOKS = gql`
   query {
     allBooks {
       title
-      author
+      author {
+        name
+      }
       published
     }
   }
@@ -35,7 +37,9 @@ export const ADD_BOOK = gql`
     ) {
       title
       published
-      author
+      author {
+        name
+      }
       genres
       id
     }
