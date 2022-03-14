9e736bdd1918a04e246b1ce1bab4739b5cdb1660
Hyeeun Mok <hyeeuni.mok@gmail.com>
2022-03-14 15:35:16 -0400

    finish ex.8.18 (8.17 was done with 8.16)

--- part8-new/library-frontend/src/App.js
+++ part8-new/library-frontend/src/App.js
@@ -1,11 +1,16 @@
 import { useState } from 'react';
+import { useApolloClient } from '@apollo/client';
 import Authors from './components/Authors';
 import Books from './components/Books';
 import NewBook from './components/NewBook';
+import LoginForm from './components/LoginForm';
 
 const App = () => {
   const [page, setPage] = useState('authors');
   const [errorMessage, setErrorMessage] = useState(null);
+  const [token, setToken] = useState(null);
+
+  const client = useApolloClient();
 
   const notify = message => {
     setErrorMessage(message);
@@ -14,17 +19,37 @@ const App = () => {
     }, 5000);
   };
 
+  const logout = () => {
+    setToken(null);
+    localStorage.clear();
+    client.resetStore();
+    setPage('authors');
+  };
+
   return (
     <div>
       <div>
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
-        <button onClick={() => setPage('add')}>add book</button>
+        {token ? (
+          <>
+            <button onClick={() => setPage('add')}>add book</button>
+            <button onClick={logout}>logout</button>
+          </>
+        ) : (
+          <button onClick={() => setPage('login')}>login</button>
+        )}
       </div>
       {errorMessage ? <div>{errorMessage}</div> : null}
       <Authors show={page === 'authors'} setError={notify} />
       <Books show={page === 'books'} />
       <NewBook show={page === 'add'} setError={notify} />
+      <LoginForm
+        show={page === 'login'}
+        setPage={setPage}
+        setError={setErrorMessage}
+        setToken={setToken}
+      />
     </div>
   );
 };
